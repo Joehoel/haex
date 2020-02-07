8306cd4bfcb0ef57521cba6ec308be13e6bcc641
Jampottie <j.mooibroek2002@gmail.com>
2020-02-07 11:34:31 +0100

    testing

---
+++ content.js
@@ -0,0 +1,16 @@
+const img = document.createElement('img')
+img.innerText = 'hello from timo'
+document.body.appendChild(img)
+
+var port = chrome.runtime.connect();
+
+window.addEventListener("message", function(event) {
+  // We only accept messages from ourselves
+  if (event.source != window)
+    return;
+
+  if (event.data.type && (event.data.type == "FROM_PAGE")) {
+    console.log("Content script received: " + event.data.text);
+    port.postMessage(event.data.text);
+  }
+}, false);
