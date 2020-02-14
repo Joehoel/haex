5bda617afdf724ca7994e954df7e6594b7adaa60
Jampottie <j.mooibroek2002@gmail.com>
2020-02-14 11:43:20 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,3 +1,8 @@
+//================================//
+// Dit is de "timo" class, als je //
+// nu google.com laad zie je hem  //
+// als het goed is op het scherm  //
+//================================//
 class Timo {
 	constructor() {
 		this.x = "";
@@ -8,10 +13,18 @@ class Timo {
 		const path = chrome.extension.getURL("Animations/timo-slow.gif");
 		const img = document.createElement("img");
 		img.src = path;
-		img.classList.add('timo')
+		img.classList.add("timo");
 		document.body.appendChild(img);
 	}
+
+	getXY(element) {
+		const rect = element.getBoundingClientRect();
+		console.log("X: " + rect.x + ", " + "Y: " + rect.y);
+	}
+
+	move() {}
 }
 
 const timo = new Timo();
 timo.show();
+timo.getXY(document.querySelector('.timo'))
