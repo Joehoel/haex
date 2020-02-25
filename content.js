d06c25cb101a49f4040fb1c28c5ba72077d65e58
Joel Kuijper <jmkuijper@outlook.com>
2020-02-25 10:46:51 +0100

    Still not working

--- content.js
+++ content.js
@@ -1,35 +1,36 @@
-//================================//
-// Dit is de "timo" class, als je //
-// nu google.com laad zie je hem  //
-// als het goed is op het scherm  //
-//================================//
 class Timo {
 	constructor() {
+		this.ctx = this.createCanvas()
 	}
 
 	show() {
 		const path = chrome.extension.getURL("Animations/timo-slow.gif");
-		const img = new Image()
+		const img = document.createElement('img')
 		img.src = path;
 		// img.classList.add("timo");
 		// document.body.appendChild(img);
-		
+
+		window.onload = () => {
+			this.ctx.drawImage(img, 100, 100);
+		}
+	}
+	
+	createCanvas() {
 		const canvas = document.createElement("canvas");
 		canvas.classList.add("c");
+	
 		document.body.appendChild(canvas);
-		const ctx = canvas.getContext("2d");
-		img.onload = ctx.drawImage(img, 0, 0);
-		
+		return canvas.getContext("2d");
 	}
 
-	getXY(element) {
-		const rect = element.getBoundingClientRect();
-		console.log("X: " + rect.x + ", " + "Y: " + rect.y);
-	}
+	// getXY(element) {
+	// 	const rect = element.getBoundingClientRect();
+	// 	console.log("X: " + rect.x + ", " + "Y: " + rect.y);
+	// }
 
-	move() {}
+	move() { }
 }
 
 const timo = new Timo();
 timo.show();
-timo.getXY(document.querySelector(".timo"));
+// timo.getXY(document.querySelector(".timo"));
