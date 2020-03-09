1eec500f37f1f1edfe22ddb0b5d96e769318df81
Joehoel <jmkuijper@Outlook.com>
2020-03-09 16:56:04 +0100

    Update content.js

--- content.js
+++ content.js
@@ -7,25 +7,26 @@ class Timo {
     this.img = document.createElement("img");
     this.path = chrome.extension.getURL("Animations/timo-slow.gif");
     this.img.src = this.path;
-    this.canvas.width = this.img.width;
-    this.canvas.height = this.img.height;
+    // this.canvas.width = this.img.width;
+    // this.canvas.height = this.img.height;
 
-    this.ctx = this.createContext();
+    // this.ctx = this.createContext();
 
     this.draw();
   }
 
   draw() {
-    window.onload = () => {
-      this.ctx.drawImage(this.img, 0, 0);
-    };
+    this.img.classList.add("timo");
+    document.body.appendChild(this.img);
+    // window.onload = () => {
+    //   this.ctx.drawImage(this.img, 0, 0);
+    // };
   }
 
   createContext() {
-    this.canvas.classList.add("c");
-
-    document.body.appendChild(this.canvas);
-    return this.canvas.getContext("2d");
+    // this.canvas.classList.add("c");
+    // document.body.appendChild(this.canvas);
+    // return this.canvas.getContext("2d");
   }
 
   move() {
