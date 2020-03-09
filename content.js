4e2d229251740e3fc750f8c0dd8dc32f552a9775
Joehoel <jmkuijper@Outlook.com>
2020-03-09 16:57:37 +0100

    Lot of things

--- content.js
+++ content.js
@@ -1,32 +1,27 @@
-// Werkt niet op alle sites
-// Het werkt wel op https://developer.chrome.com/home
-
 class Timo {
   constructor() {
-    this.canvas = document.createElement("canvas");
-    this.img = document.createElement("img");
-    this.path = chrome.extension.getURL("Animations/timo-slow.gif");
-    this.img.src = this.path;
-    // this.canvas.width = this.img.width;
-    // this.canvas.height = this.img.height;
-
-    // this.ctx = this.createContext();
-
-    this.draw();
+    this.ctx = this.createCanvas();
   }
 
-  draw() {
-    this.img.classList.add("timo");
-    document.body.appendChild(this.img);
-    // window.onload = () => {
-    //   this.ctx.drawImage(this.img, 0, 0);
-    // };
+  show() {
+    const path = chrome.extension.getURL("Animations/timo-slow.gif");
+    const img = document.createElement("img");
+    img.src = path;
+    img.classList.add("timo");
+    document.body.appendChild(img);
+
+    // ! Werkt niet
+    window.onload = () => {
+      this.ctx.drawImage(img, 100, 100);
+    };
   }
 
-  createContext() {
-    // this.canvas.classList.add("c");
-    // document.body.appendChild(this.canvas);
-    // return this.canvas.getContext("2d");
+  createCanvas() {
+    const canvas = document.createElement("canvas");
+    canvas.classList.add("c");
+
+    document.body.appendChild(canvas);
+    return canvas.getContext("2d");
   }
 
   move() {
@@ -35,4 +30,5 @@ class Timo {
 }
 
 const timo = new Timo();
+timo.show();
 // timo.getXY(document.querySelector(".timo"));
