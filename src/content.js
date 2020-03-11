27c91d1f896e7176531296a0b92368babed135e1
Joehoel <jmkuijper@Outlook.com>
2020-03-11 21:19:32 +0100

    adwda

---
+++ src/content.js
@@ -0,0 +1,12 @@
+replaceText(document.body);
+
+function replaceText(element) {
+  if (element.hasChildNodes()) {
+    element.childNodes.forEach(replaceText);
+  } else if (element.nodeType === Text.TEXT_NODE) {
+    if (element.textContent.match(/corona/gi)) {
+      element.parentElement.style.color = "black";
+      element.parentElement.style.backgroundColor = "black";
+    }
+  }
+}
