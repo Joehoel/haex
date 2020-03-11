7edd09cac64f054e048b84de77176641a96a84f0
Joehoel <jmkuijper@Outlook.com>
2020-03-11 21:56:48 +0100

    Update content.js

--- src/content.js
+++ src/content.js
@@ -4,9 +4,18 @@ function replaceText(element) {
   if (element.hasChildNodes()) {
     element.childNodes.forEach(replaceText);
   } else if (element.nodeType === Text.TEXT_NODE) {
-    if (element.textContent.match(/corona/gi)) {
-      element.parentElement.style.color = "black";
-      element.parentElement.style.backgroundColor = "black";
+    if (containsText(element, "corona")) {
+      element.textContent = element.textContent.replace(/corona/gi, "fruit");
+    }
+    if (containsText(element, "COVID")) {
+      element.textContent = element.textContent.replace(
+        /COVID\-19/gi,
+        "FROVID-21"
+      );
     }
   }
+}
+function containsText(element, text) {
+  const regex = new RegExp(text, "gi");
+  return element.textContent.match(regex);
 }
