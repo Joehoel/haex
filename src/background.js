1ab1ba126eafae592c3382ca3a2435a291fff1cd
Joehoel <jmkuijper@Outlook.com>
2020-03-11 21:07:02 +0100

    dawdawa

--- src/background.js
+++
@@ -1,4 +0,0 @@
-// var img = document.createElement("img");
-// img.src = chrome.extension.getURL('Animations/timo-slow.gif');
-// var src = document.getElementById("header");
-// src.appendChild(img);
