49276a563f0e63e2c7f3250347675088ada63433
Jampottie <j.mooibroek2002@gmail.com>
2020-03-13 10:40:58 +0100

    aw,md aw,dd

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,6 @@
 const loadHaexBtn = document.getElementById("loadhaex");
 
-function GetSiteName(){
-  var SiteName = document.getElementById('ctl00_PlaceHolderSiteName_onetidProjectPropertyTitle').innerHTML;
-document.getElementById('siteName').innerHTML = SiteName;
+function getSiteName() {
+  const siteName = document.getElementById('ctl00_PlaceHolderSiteName_onetidProjectPropertyTitle').innerHTML;
+  document.getElementById('siteName').innerHTML = siteName;
 }
