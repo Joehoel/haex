76de972ec9db1ddeb241236f436944d10416709f
Jampottie <j.mooibroek2002@gmail.com>
2020-03-13 10:32:22 +0100

    pop

--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,3 @@
 const loadHaexBtn = document.getElementById("loadhaex");
 
-function GetSiteName(){
-  var SiteName = document.getElementById('ctl00_PlaceHolderSiteName_onetidProjectPropertyTitle').innerHTML;
-document.getElementById('siteName').innerHTML = SiteName;
-}
+document.getElementById('siteName').innerHTML = window.location.href;
