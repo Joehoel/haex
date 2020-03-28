9e1945276e6702a05b51bf0c16d2f96de1784289
Jampottie <j.mooibroek2002@gmail.com>
2020-03-28 14:42:30 +0100

    Create startFonthack.js

---
+++ src/startFonthack.js
@@ -0,0 +1,12 @@
+/*Het hacken kan beginnen*/
+
+//Voeg de font toe aan de pagina
+var headHTML = document.getElementsByTagName('head')[0].innerHTML;
+headHTML    += '<link href="https://fonts.googleapis.com/css?family=Oxanium&display=swap" rel="stylesheet"/>';
+document.getElementsByTagName('head')[0].innerHTML = headHTML;
+
+//Vervang elke div door een gestylde div
+const divs = document.querySelectorAll("<div>");
+divs.forEach(div => {
+  div.src = "<div style='font-family: 'Oxanium', cursive;'";
+});
