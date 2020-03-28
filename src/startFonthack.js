b966d3152446bdf18f97714f82adf73fa43ed89f
Joehoel <jmkuijper@Outlook.com>
2020-03-28 21:11:58 +0100

    Update startFonthack.js

--- src/startFonthack.js
+++ src/startFonthack.js
@@ -1,15 +1,17 @@
 /*Het hacken kan beginnen*/
 
 //Voeg de font toe aan de pagina
-var headHTML = document.getElementsByTagName('head')[0].innerHTML;
-headHTML    += '<link href="https://fonts.googleapis.com/css?family=Oxanium&display=swap" rel="stylesheet"/>';
-document.getElementsByTagName('head')[0].innerHTML = headHTML;
+const headHTML = document.getElementsByTagName("head")[0].innerHTML;
+headHTML +=
+  '<link href="https://fonts.googleapis.com/css?family=Oxanium&display=swap" rel="stylesheet"/>';
+
+//! Deze line is niet nodig vgm
+// document.getElementsByTagName("head")[0].innerHTML = headHTML;
 
 //Vervang elke div door een gestylde div
-const divs = document.querySelectorAll("<div>");
+const divs = document.querySelectorAll("div");
 divs.forEach(div => {
-  div.src = "<div style='font-family: 'Oxanium', cursive;'";
+  div.style.cssText = "font-family: 'Oxanium', cursive;";
 });
-
 
 //Moet nog een delay worden ingezet
