50d865d17032679fc11639d4f3ac5861cb104334
Jampottie <j.mooibroek2002@gmail.com>
2020-03-30 20:22:48 +0200

    Update font.js

--- src/hacks/font.js
+++ src/hacks/font.js
@@ -1,17 +1,25 @@
 /*Het hacken kan beginnen*/
 
 //Voeg de font toe aan de pagina
-// const headHTML = document.getElementsByTagName("head")[0].innerHTML;
-document.head.innerHTML +=
-  '<link href="https://fonts.googleapis.com/css?family=Oxanium&display=swap" rel="stylesheet"/>';
-document.body.style.fontFamily = "'Oxanium', cursive";
+document.head.innerHTML += '<link href="https://fonts.googleapis.com/css?family=Oxanium&display=swap" rel="stylesheet"/>';
+
+//Krijg de huidige font
+var currentFont = window.getComputedStyle(document.querySelector('body')).font-family;
+
+//Vervang elke div door een gestylde div
+(function myLoop (i) {
+  setTimeout(function () {
+    alert('hello');
+    if (--i) myLoop(i);
+  }, 3000)
+})(10);
+
+//document.body.style.fontFamily = "'Oxanium', cursive";
 //! Deze line is niet nodig vgm
 // document.getElementsByTagName("head")[0].innerHTML = headHTML;
 
-//Vervang elke div door een gestylde div
-// const divs = document.querySelectorAll("div");
-// divs.forEach(div => {
-//   div.style.cssText = ;
-// });
+
 
 //Moet nog een delay worden ingezet
+
+//Nieuwe code:
