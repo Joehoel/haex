3af45eb494708edb47e63bbb8484d5bdbc680a0b
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-03-31 22:29:57 +0200

    Update font.js

--- src/hacks/font.js
+++ src/hacks/font.js
@@ -1,18 +1,19 @@
 /*Het hacken kan beginnen*/
 
 //Voeg de font toe aan de pagina
-document.head.innerHTML += '<link href="https://fonts.googleapis.com/css?family=Oxanium&display=swap" rel="stylesheet"/>';
-
-
+document.head.innerHTML +=
+  '<link href="https://fonts.googleapis.com/css?family=Oxanium&display=swap" rel="stylesheet"/>';
 
 //Krijg de huidige font
-var currentFont = window.getComputedStyle(document.querySelector('body')).getPropertyValue('font-family');
+const currentFont = window
+  .getComputedStyle(document.querySelector("body"))
+  .getPropertyValue("font-family");
 
-while(true) {  
+while (true) {
   document.body.style.fontFamily = "'Oxanium', cursive";
-  sleep(Math.floor((Math.random() * 2700) + 300));
+  sleep(Math.floor(Math.random() * 2700 + 300));
   document.body.style.fontFamily = currentFont;
-  sleep(Math.floor((Math.random() * 2700) + 300));
+  sleep(Math.floor(Math.random() * 2700 + 300));
 }
 
 function sleep(milliseconds) {
