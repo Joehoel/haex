74b001be3fc3f1964c46379f6f5167ce5ceeddf0
Joehoel <31251240+Joehoel@users.noreply.github.com>
2020-04-03 15:54:28 +0200

    Update editpage.js

--- src/hacks/editpage.js
+++ src/hacks/editpage.js
@@ -1,17 +1,19 @@
-const editingPopup = document.createElement("div");
-editingPopup.style.position = "fixed";
-editingPopup.style.zIndex = "999";
-editingPopup.style.top = "0";
-editingPopup.textContent = "Save";
-editingPopup.style.fontSize = "1.5rem";
-editingPopup.style.backgroundColor = "#303030";
-editingPopup.style.color = "white";
-editingPopup.style.border = "2px solid #03dac6";
-editingPopup.style.padding = "1rem";
-editingPopup.style.margin = "1rem";
-editingPopup.style.borderRadius = "10px";
-editingPopup.style.boxShadow = "0px 0px 17px 3px rgba(0,0,0,0.75)";
-editingPopup.style.cursor = "pointer";
+console.log("Hello from editpage.js");
+
+const saveButton = document.createElement("div");
+saveButton.style.position = "fixed";
+saveButton.style.zIndex = "999";
+saveButton.style.top = "0";
+saveButton.textContent = "Save";
+saveButton.style.fontSize = "1.5rem";
+saveButton.style.backgroundColor = "#303030";
+saveButton.style.color = "white";
+saveButton.style.border = "2px solid #03dac6";
+saveButton.style.padding = "1rem";
+saveButton.style.margin = "1rem";
+saveButton.style.borderRadius = "10px";
+saveButton.style.boxShadow = "0px 0px 17px 3px rgba(0,0,0,0.75)";
+saveButton.style.cursor = "pointer";
 
 const currentFont = window
   .getComputedStyle(document.body)
@@ -19,11 +21,11 @@ const currentFont = window
 
 document.body.contentEditable = "true";
 document.designMode = "on";
-document.body.appendChild(editingPopup);
+document.body.appendChild(saveButton);
 document.body.style.fontFamily = currentFont;
 
-editingPopup.addEventListener("click", () => {
+saveButton.addEventListener("click", () => {
   document.body.contentEditable = "false";
   document.designMode = "off";
-  editingPopup.remove();
+  document.body.removeChild(saveButton);
 });
